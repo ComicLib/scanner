696641121b9288d031f1f59349df821177d24ed2
URenko <18209292+URenko@users.noreply.github.com>
2024-02-05 16:51:37 +0800

    add 3x-translate.py

---
+++ 3x-translate.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+import requests
+
+class Scanner:
+    '''Translate English tags to Chinese'''
+
+    def __init__(self) -> None:
+        tag_trans_db = requests.get('https://cdn.jsdelivr.net/gh/EhTagTranslation/DatabaseReleases/db.text.json').json()["data"]
+        self.trans_tag = {}
+        self.trans_namespace = {}
+        for namespace in tag_trans_db:
+            self.trans_namespace[namespace["namespace"]] = namespace["frontMatters"]["name"]
+            for tag in namespace["data"]:
+                self.trans_tag[f'{namespace["namespace"]}:{tag}'] = f'{namespace["frontMatters"]["name"]}:{namespace["data"][tag]["name"]}'
+    
+    def scan(self, path: Path, id: str, metadata: dict, prev_scanners: list[str]) -> bool:
+        if metadata["tags"] is None:
+            return False
+        old_tags = metadata["tags"]
+        tags = set()
+        for tag in old_tags:
+            if tag.isascii() and tag in self.trans_tag:
+                tags.add(self.trans_tag[tag])
+            else:
+                tags.add(tag)
+        metadata["tags"] = tags
+        return not tags == old_tags
