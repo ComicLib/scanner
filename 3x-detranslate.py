6abbd19a72279888e507824b90c7ace485dbb901
URenko <18209292+URenko@users.noreply.github.com>
2024-02-05 16:53:09 +0800

    3x-detranslate: handle old translations

--- 3x-detranslate.py
+++ 3x-detranslate.py
@@ -5,13 +5,9 @@ class Scanner:
     '''Translate Chinese tags back to English'''
 
     def __init__(self) -> None:
-        tag_trans_db = requests.get('https://cdn.jsdelivr.net/gh/EhTagTranslation/DatabaseReleases/db.text.json').json()["data"]
-        self.detrans_tag = {}
-        self.detrans_namespace = {}
-        for namespace in tag_trans_db:
-            self.detrans_namespace[namespace["frontMatters"]["name"]] = namespace["namespace"]
-            for tag in namespace["data"]:
-                self.detrans_tag[f'{namespace["frontMatters"]["name"]}:{namespace["data"][tag]["name"]}'] = f'{namespace["namespace"]}:{tag}'
+        zh2en = requests.get("https://cdn.jsdelivr.net/gh/ComicLib/EhTagTranslation_rev/zh2en.json").json()
+        self.detrans_namespace = zh2en["namespace"]
+        self.detrans_tag = zh2en["tag"]
     
     def scan(self, path: Path, id: str, metadata: dict, prev_scanners: list[str]) -> bool:
         if metadata["tags"] is None:
@@ -21,12 +17,9 @@ class Scanner:
         for tag in old_tags:
             if not tag.isascii():
                 namespace, _, name = tag.partition(':')
-                if namespace == '社团': namespace = "团队"
-                if name.isascii():
-                    tags.add(self.detrans_namespace[namespace] + ':' + name)
-                else:
-                    tags.add(self.detrans_tag[namespace + ':' + name])
-            else:
-                tags.add(tag)
+                if namespace in self.detrans_namespace:
+                    tag = self.detrans_namespace[namespace] + ':' + self.detrans_tag[self.detrans_namespace[namespace]].get(name, name)
+            assert tag.isascii(), repr(tag)
+            tags.add(tag)
         metadata["tags"] = tags
         return not tags == old_tags
